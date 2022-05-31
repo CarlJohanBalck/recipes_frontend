c3e09c2eeebfbf005db8ac2976303eb7f6ddbd27
Carl Johan Balck <carl-johan.balck@tactel.se>
2022-05-31 16:36:00 +0200

    Added fader to images

--- src/components/Card.js
+++ src/components/Card.js
@@ -21,9 +21,12 @@ const Card = ({recepie, index, onClick}) => {
   return (
     <div className="card" key={recepie.id}>
         <div className="card-image">
+          <div class="fade"></div>
             <img src={genericfood} alt=''/>
+            
             {/* <img src={Soyalax} alt={recepie.title}/> */}
             <span className="card-title">{recepie[0].replace(/ *\([^)]*\) */g, "").slice(0, -1)}</span>
+            
             <span to="/" className="btn-floating halfway-fab waves-effect waves-light red" onClick={() => handleClick(recepie, selected)}><i className="material-icons">add</i></span>
         </div>
 
