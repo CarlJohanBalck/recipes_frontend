961699e074a42964ce09c2880231a965741d0085
Carl Johan Balck <carl-johan.balck@tactel.se>
2022-08-09 09:27:28 +0200

    Update ip port

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.pi_address = "http://192.168.0.4:5001"
+config.pi_address = "http://192.168.0.4:5002"
 config.pi_get_recepies = config.pi_address + "/Siri/Recepies"
 config.pi_post_selection = config.pi_address + "/Siri/ReactRecepies"
 config.mock_api = "http://localhost:3000/"
